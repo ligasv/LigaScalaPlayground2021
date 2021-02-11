bbe0318e8fb482751073c773764cc1197b1ad288
ligasv <liga.svile@gmail.com>
2021-02-11 20:01:41 +0200

    nu šis man nesanāca

---
+++ src/main/scala/functionwithexLiga.scala
@@ -0,0 +1,17 @@
+object functionwithexLiga extends App {
+def goEat():Unit = {
+   println("go eat")
+  println("let's order food")
+}
+def orderFood(disch: String): Unit = {
+  println(s"i' ordering $dish")
+  println(s"${dish.toUpperCase} shoud be prettu tasty")
+}
+  orderFood(disch = "potatoes")
+  orderFood(disch = "ice cream")
+  val soup= "beet soup"
+  def eat(foodlist: Seq[String]: Unit = {
+    println("greetings")
+    printlin
+  }
+}
